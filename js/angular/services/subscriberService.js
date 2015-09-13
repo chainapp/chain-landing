6be30127d9cc7ef4b9027af9a5996ca49928ff2b
Philippe Braun <p.marinbaun@gmail.com>
2015-09-14 01:13:13 +0200

    Ajout des logos en SVG, modification des couleurs

--- js/angular/services/subscriberService.js
+++ js/angular/services/subscriberService.js
@@ -5,7 +5,7 @@ this.subscribeCall = function(name,mail) {
   console.log('in subscriber service')
   $http({
     method: 'GET',
-    url: 'http://localhost:8081/subscribe?name='+name+'&mail='+mail,
+    url: 'http://facefight-landing.elasticbeanstalk.com:8081/subscribe?name='+name+'&mail='+mail,
     //url: 'http://lb.qlf-waas.priv.atos.fr:8068/getArret',
     headers: {'Content-type':'application/json'}
   }).
